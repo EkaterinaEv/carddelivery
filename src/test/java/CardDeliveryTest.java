633c5a83b200d66df79358aaf02badedaadba521
Ekaterina Evtyukhova <katyev60@gmail.com>
2023-09-20 22:22:12 +0300

    testupdate commit

--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -1,21 +1,35 @@
-import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class CardDeliveryTest {
+    private String generateDate(int addDays, String pattern) {
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
+    }
+
     @Test
     void shouldTest() {
         open("http://localhost:9999");
-        SelenideElement form = $("[action]");
-        form.$("[data-test-id=city] input").setValue("Москва");
-        form.$("[data-test-id=date] input").setValue("21.09.2023");
-        form.$("[data-test-id=name] input").setValue("Екатерина Е");
-        form.$("[data-test-id=phone] input").setValue("+79110000000");
-        form.$("[data-test-id=agreement]").click();
-        form.$("[role]").click();
-        form.$("[data-test-id=notification]");
+        $("[data-test-id=city] input").setValue("Москва");
+        String planningDate = generateDate(4, "dd.MM.yyyy");
+        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id=date] input").setValue(planningDate);
+        $("[data-test-id=name] input").setValue("Екатерина Е");
+        $("[data-test-id=phone] input").setValue("+79110000000");
+        $("[data-test-id=agreement]").click();
+        $("button.button").click();
+        $(".notification__content")
+                .shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldHave(Condition.exactText("Встреча успешно забронирована на " + planningDate));
+
     }
+
 }
 
